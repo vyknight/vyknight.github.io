b491e6fc7c8404855ba7a8fdb936873ac8d8af39
vyknight <knightadark101@gmail.com>
2023-07-10 16:30:21 -0600

    Updates

---
+++ output/server/manifest-full.js
@@ -0,0 +1,34 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "vyknight.github.io/_app",
+	assets: new Set([".nojekyll","favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.6ee8707f.js","app":"_app/immutable/entry/app.eb8fa769.js","imports":["_app/immutable/entry/start.6ee8707f.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.c76dc377.js","_app/immutable/entry/app.eb8fa769.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.0719bd3d.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+}
+})();
