ff9bc24de1678599613d686b87bcb942afec8817
vyknight <knightadark101@gmail.com>
2023-07-10 16:53:52 -0600

    Copy Changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>This is the personal page of VYKnight</h1>
+<p>This website is built with Svelte using SvelteKit</p>
+<p>Due to me not knowing Svelte, this site will be in continuous development</p>
